66aea440853c0b7be925767057b983e3da283c11
Srideep Musuvathy <smusuva@sandia.gov>
2019-05-31 15:01:06 -0600

    added comments to sushi_chef interface file

--- code/backends/sushi_chef.py
+++ code/backends/sushi_chef.py
@@ -16,10 +16,16 @@ def serve_fugu_to_snn(fugu_circuit, fugu_graph, n_steps=1, record_all=False, ds_
     nn = snn.NeuralNetwork()
     neuron_dict = {}
     
+    #if compatibility with ds simulator is needed, add neuron numbers
     if ds_format:
         for i, n in enumerate(fugu_graph.nodes):
            fugu_graph.nodes[n]['neuron_number'] = i
     
+    ''' Add Neurons '''
+    #Add in input and output neurons. Use the fugu_circuit information to identify input and output layers
+    #For input nodes, create input neurons and identity and associate the appropriate inputs to it
+    #for output neurons, obtain neuron parameters from fugu_graph and create LIFNeurons
+    #Add neurons to spiking neural network
     for node, vals in fugu_circuit.nodes.data():
         if 'layer' in vals: 
             if vals['layer'] == 'input':
@@ -44,7 +50,8 @@ def serve_fugu_to_snn(fugu_circuit, fugu_graph, n_steps=1, record_all=False, ds_
                             lk = 1.0 - params['decay']
                         neuron_dict[neuron] = snn.LIFNeuron(neuron, threshold=th, reset_voltage=rv, leakage_constant=lk, voltage=vol, record=True)
                         nn.add_neuron(neuron_dict[neuron])
-                        
+    #add other neurons from fugu_graph to spiking neural network
+    #parse through the fugu_graph and if a neuron is not present in spiking neural network, add to it.                    
     for neuron, params in fugu_graph.nodes.data():
         if neuron not in neuron_dict.keys():
             th = params.get('threshold', 0.0)
@@ -58,7 +65,9 @@ def serve_fugu_to_snn(fugu_circuit, fugu_graph, n_steps=1, record_all=False, ds_
             rc = True if record_all else params.get('record', False)
             neuron_dict[neuron] = snn.LIFNeuron(neuron, threshold=th, reset_voltage=rv, leakage_constant=lk, voltage=vol, record=rc)
             nn.add_neuron(neuron_dict[neuron])
-            
+    
+    ''' Add Synapses '''
+    #add synapses from fugu_graph edge information        
     for n1, n2, params in fugu_graph.edges.data():
         delay = int(params.get('delay', 1))
         wt = params.get('weight', 1.0)
@@ -66,21 +75,28 @@ def serve_fugu_to_snn(fugu_circuit, fugu_graph, n_steps=1, record_all=False, ds_
         nn.add_synapse(syn)
     
     del neuron_dict
+    
+    ''' Run the Simulator '''
     df = nn.run(n_steps=n_steps, debug_mode=False)
     res = {}
+    #if ds format is required, convert neuron names to numbers and return dictionary
+    #else return dataframe
     if ds_format:
         numerical_cols = {}
         for c in df.columns:
             numerical_cols[c] = fugu_graph.nodes[c]['neuron_number']
         df = df.rename(index=int, columns=numerical_cols) 
-    for r in df.index:
-        l = []
-        for c in df.columns:
-            if df.loc[r][c] == 1:
-                l.append(c)
-        res[r] = l
+        
+        for r in df.index:
+            l = []
+            for c in df.columns:
+                if df.loc[r][c] == 1:
+                    l.append(c)
+                res[r] = l
 
-    return res
+        return res
+    else:
+        return df
     
     
     
