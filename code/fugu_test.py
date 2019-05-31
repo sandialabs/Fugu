a00f16de97ad2ca96aafb6e8281602a734d7b739
Srideep Musuvathy <smusuva@sandia.gov>
2019-05-31 11:32:18 -0600

    adding interface and test files

---
+++ code/fugu_test.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 13:02:07 2019
+
+@author: smusuva
+"""
+
+import networkx as nx
+import numpy as np
+import fugu
+from fugu import Scaffold, Brick, Spike_Input
+
+
+class basic_AND(Brick):
+    def __init__(self, name=None):
+        super(Brick, self).__init__()
+        #The brick hasn't been built yet.
+        self.is_built = False
+        #Leave for compatibility, D represents the depth of the circuit.  Needs to be updated.
+        self.metadata = {'D':1}  
+        #We just store the name passed at construction.
+        self.name = name
+        #For this example, we'll let any input coding work even though the answer might not make sense.
+        self.supported_codings = fugu.input_coding_types
+    def build(self,
+             graph,
+             metadata,
+             control_nodes,
+             input_lists,
+             input_codings):
+        #Expect two inputs
+        if len(input_codings)!=2:
+            raise ValueError('Only two inputs supported.')
+        #Keep the same coding as input 0 for the output
+        #This is an arbitrary decision at this point.  
+        #Generally, your brick will impart some coding, but that isn't the case here.
+        output_codings = [input_codings[0]]
+        
+        #All bricks should provide a neuron that spikes when the brick has completed processing.
+        #We just put in a basic relay neuron that will spike when it receives any spike from its
+        #single input, which is the complete_node from the first input.
+        #All nodes we add to the graph should have basic neuron parameters (threshold, decay)
+        #Reasonable defaults will be filled-in, but these defaults may depend on the execution platform.
+        #Additionally, nodes should have a field called 'index' which is a local index used to reference the 
+        #position of the node.  This can be used by downstream bricks.  A simple example might be
+        #a 3-bit binary representation will add 3 nodes to the graph with indices 0,1,2
+        #We do have to do some work to establish best practices here.
+        new_complete_node_name = self.name + '_complete'
+        graph.add_node(new_complete_node_name,
+                      index = -1,
+                      threshold = 0.0,
+                      decay =0.0,
+                      p=1.0,
+                      potential=0.0)
+        graph.add_edge(control_nodes[0]['complete'], new_complete_node_name,weight=1.0,delay=1)
+        
+        #We also, obviously, need to build the computational portion of our graph
+        and_node_name = self.name + '_0'
+        graph.add_node(and_node_name,
+                       index=0,
+                       threshold=1.0,
+                       decay=1.0,
+                       p=1.0,
+                       potential=0.0
+                      )
+        graph.add_edge(input_lists[0][0],
+                      and_node_name,
+                      weight=0.75,
+                      delay=1.0)
+        graph.add_edge(input_lists[1][0],
+                      and_node_name,
+                      weight=0.75,
+                      delay=1.0)
+        self.is_built=True
+        
+        #Remember, bricks can have more than one output, so we need a list of list of output neurons
+        output_lists = [[and_node_name]]
+        
+        return (graph,
+               self.metadata,
+                [{'complete':new_complete_node_name}],
+                output_lists,
+                output_codings
+               )
+
+scaffold = Scaffold()
+scaffold.add_brick(Spike_Input(np.array([1]), coding='Raster', name='Input0'), 'input' )
+scaffold.add_brick(Spike_Input(np.array([1]), coding='Raster', name='Input1'), 'input' )
+scaffold.add_brick(basic_AND(name='AND'), [(0,0), (1,0)], output=True)
+scaffold.lay_bricks()
+result = scaffold.evaluate(backend='snn', record_all=False)
+print(result)
