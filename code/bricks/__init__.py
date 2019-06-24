1e856b20be38ac573a0b18ac3f561065fdf99f13
Srideep Musuvathy <smusuva@sandia.gov>
2019-06-24 14:55:16 -0600

    organized bricks and scaffold into their own folders

---
+++ code/bricks/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 14:16:57 2019
+
+@author: smusuva
+"""
+
+from bricks import Brick, InputBrick, Vector_Input, Spike_Input, PRN, 
+Threshold, Dot, Copy, Concatenate, AND_OR, Shortest_Path, Breadth_First_Search, 
+ParityCheck, LongestIncreasingSubsequence 
+
