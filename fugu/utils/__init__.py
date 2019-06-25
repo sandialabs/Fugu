e0fdd947dc3ce1b91a747c34c3aa456da423fd98
Srideep Musuvathy <smusuva@sandia.gov>
2019-06-25 16:09:36 -0600

    created installer

---
+++ fugu/utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 15:06:25 2019
+
+@author: smusuva
+"""
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 15:56:33 2019
+
+@author: smusuva
+"""
+from setuptools import setup, find_packages
+
+
+setup(name='fugu', 
+      version='0.1', 
+      description='A python library for computational neural graphs',
+      packages=find_packages())
+
