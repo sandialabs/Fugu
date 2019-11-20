282c6e9378c9bcb215ea08f6b67bb0cea2683264
J. Darby Smith <jsmit16@sandia.gov>
2019-11-20 15:07:41 -0700

    changed .node to .nodes where necessary

--- fugu/backends/ds.py
+++ fugu/backends/ds.py
@@ -47,8 +47,8 @@ def remove_delay(graph, verbose = 0):
             if old_delay > 1:
                 to_add_neuron = graph.number_of_nodes()
                 graph.add_node(to_add_neuron)
-                graph.node[to_add_neuron]['potential'] = 0.0
-                graph.node[to_add_neuron]['threshold'] = 0.5
+                graph.nodes[to_add_neuron]['potential'] = 0.0
+                graph.nodes[to_add_neuron]['threshold'] = 0.5
                 graph.add_edge(from_neuron, to_add_neuron, weight=1.0, delay=1)
                 graph.add_edge(to_add_neuron, to_neuron, weight=graph[from_neuron][to_neuron]['weight'], delay=old_delay-1)
                 graph.remove_edge(edge[0], edge[1])
@@ -61,7 +61,7 @@ def restore_delay(graph):
     while not done:
         edges_list = list(graph.edges())
         for edge in edges_list:
-            if graph.out_degree(edge[1])==1 and graph[edge[0]][edge[1]]['weight'] > graph.node[edge[1]]['threshold']:
+            if graph.out_degree(edge[1])==1 and graph[edge[0]][edge[1]]['weight'] > graph.nodes[edge[1]]['threshold']:
                 target_neuron = list(graph.neighbors(edge[1]))[0]
                 first_edge = graph[edge[0]][edge[1]].copy()
                 next_edge = graph[edge[1]][target_neuron].copy()
@@ -83,7 +83,7 @@ def restore_delay(graph):
 def SpikingPDE_to_ds_graph(graph, neuron_list):
     graph_to_return = graph.copy()
     for neuron in neuron_list:
-        node = graph_to_return.node[neuron.name]
+        node = graph_to_return.nodes[neuron.name]
         for var in vars(neuron):
             node[var] = vars(neuron)[var]
     graph_to_return.graph['has_delay'] = False
