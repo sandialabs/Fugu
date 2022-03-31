8aa2277d296c93bc7d509b7d92ba267ad4e6b8c2
Ingrid Coleen Lane <iclane@sandia.gov>
2022-03-31 17:39:49 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx.ext.githubpages',
+    'sphinx_rtd_theme'
 ]
 
 
@@ -62,7 +63,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
