cda5ea71451156c87227663ca733890124e531ab
William Mark Severa <wmsever@sandia.gov>
2022-05-17 15:47:20 -0600

    Update __init__.py to remove Parity and Threshold

--- fugu/bricks/__init__.py
+++ fugu/bricks/__init__.py
@@ -3,10 +3,10 @@
 
 from .bricks import Brick
 from .input_bricks import InputBrick, Vector_Input
-from .stochastic_bricks import PRN, Threshold
+from .stochastic_bricks import PRN
 from .application_bricks import LIS
 from .graph_bricks import SimpleGraphTraversal, RegisterGraphTraversal, FlowAugmentingPath
-from .utility_bricks import Dot, Copy, Concatenate, AND_OR, ParityCheck, TemporalAdder
+from .utility_bricks import Dot, Copy, Concatenate, AND_OR, TemporalAdder
 from .register_bricks import Register, Max, Addition, Subtraction
 from .test_bricks import InstantDecay, SynapseProperties, SumOfMaxes
 from .adder_bricks import streaming_adder, temporal_shift, streaming_scalar_multiplier
