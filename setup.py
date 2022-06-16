17ae4a1dce8e4fc85f617b1f3dea79526c9acf46
SNL-NERL <cmviney@sandia.gov>
2022-06-16 13:01:41 -0600

    Initial release of Sandia Labs Fugu spiking neural algorithm tool

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+package_list = find_packages()
+
+setup(name='fugu',
+      version='0.1',
+      description='A python library for computational neural graphs',
+      install_requires = ['decorator',
+                  'future',
+                  'greenlet',
+                  'msgpack',
+                  'pandas>1.0.4',
+                  'python-dateutil',
+                  'pytz',
+                  'networkx==2.4',
+                  'numpy~=1.19.0',
+                  'six==1.15.0'
+                  ],
+      packages=package_list)
