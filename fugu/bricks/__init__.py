533c4515b7a53af862dbe161c2d33f64be8ea3f7
SNL-NERL <cmviney@sandia.gov>
2023-04-06 23:36:04 -0600

    Updating Fugu brick

--- fugu/bricks/__init__.py
+++ fugu/bricks/__init__.py
@@ -3,7 +3,7 @@
 
 from .bricks import Brick
 from .input_bricks import InputBrick, Vector_Input
-from .stochastic_bricks import PRN
+from .stochastic_bricks import PRN, Threshold
 from .application_bricks import LIS
 from .graph_bricks import SimpleGraphTraversal, RegisterGraphTraversal, FlowAugmentingPath
 from .utility_bricks import Dot, Copy, Concatenate, AND_OR, TemporalAdder
