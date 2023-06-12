45a36ecd0d9e2835cedc921402d11294e8175fa3
Michael C. Krygier <mkrygie@sandia.gov>
2023-06-12 17:41:26 -0600

    Implemented 1D/2D Dense layer bricks  - Added 1D/2D dense layer brick to repository. Will add unit tests for these bricks next.  - Added unit tests for 1D dense layer brick.  - Addded 2D dense layer brick unit test.  - Fixed bug in 2d dense layer brick where the synapse weights (edges) were not be applied to properly.  - Added a new parameter to the dense layer constructor that allows the user to overide the previous layer's prefix in the metadata. For instance, by default the dense layer brick looks for the dictionary key 'pooling_output_shape' in the metadata to assign the 'dense_input_shape' dictionary value. This new parameter in the constructor allows for you to override this default value. For example, using 'dense_' in the parameter value will look for a 'dense_output_shape' dictionary key in the metadata.  - Added two additional dense layer brick unit tests as examples.  - Expanding dense layer brick functionality to allow for different size input / output nodes. The dense layer brick constructor takes an additional argument that specifies the output neuron shape.  - Added TODO marks to the code and unit tests for the remaining items to do.  - Added additional unit tests  - Minor edits to 2d dense layer brick.

--- fugu/bricks/__init__.py
+++ fugu/bricks/__init__.py
@@ -12,4 +12,4 @@ from .test_bricks import InstantDecay, SynapseProperties, SumOfMaxes
 from .adder_bricks import streaming_adder, temporal_shift, streaming_scalar_multiplier
 from .convolution_bricks import convolution_1d, convolution_2d
 from .pooling_bricks import pooling_1d, pooling_2d
-
+from .dense_bricks import dense_layer_1d, dense_layer_2d
