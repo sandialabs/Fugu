7c61931c1a644e2e80425d2f2d4da9625ed20443
Michael C. Krygier <mkrygie@sandia.gov>
2023-11-07 16:20:51 -0700

    Removed BatchNorm string from whetstone 2 fugu unit test. No longer separating out unit tests based on the use of batchnormalization or not.

--- tests/unit/bricks/test_keras_convolution_brick.py
+++ tests/unit/bricks/test_keras_convolution_brick.py
@@ -8,6 +8,8 @@ from fugu.bricks.keras_convolution_bricks import keras_convolution_2d as convolu
 from fugu.bricks.input_bricks import BaseP_Input
 from fugu.scaffold import Scaffold
 
+from scipy.signal import convolve2d
+
 class Test_KerasConvolution2D:
     def setup_method(self):
         self.basep = 2
@@ -242,6 +244,18 @@ class Test_KerasConvolution2D:
         assert self.expected_spikes(nSpikes) == self.calculated_spikes(thresholds,result)
 
     @pytest.mark.xfail(reason="Not implemented.")
+    def test_handling_of_4d_tensor_input(self):
+        self.basep = 2
+        self.bits = 2
+        self.pvector = generate_mock_image(2,2,2)
+        self.filters = generate_keras_kernel(2,2,3,2)
+        self.pshape = np.array(self.pvector).shape
+        self.filters_shape = np.array(self.filters).shape
+        self.mode = "same"
+        self.strides = (1,1)
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
     def test_4d_tensor_input(self):
         self.basep = 2
         self.bits = 2
@@ -362,3 +376,74 @@ def get_unique_thresholds(image,kernel,strides,mode,nSpikes):
     thresholds = strided_answer - subt
 
     return thresholds
+
+# Auxillary/Helper functions
+def keras_convolve2d(image,kernel,strides=(1,1),mode="same"):
+    '''
+        Custom function that uses scipy.signal.convove2d to reproduce the Keras 2D convolution result with strides
+    '''
+    return np.flip(convolve2d(np.flip(image),np.flip(kernel),mode=mode))[::strides[0],::strides[1]]
+
+def keras_convolve2d_4dinput(image,kernel,strides=(1,1),mode="same",data_format="channels_last",filters=1):
+    '''
+        Custom function that uses scipy.signal.convove2d to reproduce the Keras 2D convolution result with strides. Attempts to
+        handle the number of channels in image and the number of kernels per filter to calculating the result.
+    '''
+    if data_format.lower() == "channels_last":
+        if image.ndim == 3:
+            height, width, nChannels = image.shape
+        elif image.ndim == 4:
+            batch_size, height, width, nChannels = image.shape
+    elif data_format.lower() == "channels_first":
+        if image.ndim == 3:
+            nChannels, height, width = image.shape
+        elif image.ndim == 4:
+            batch_size, nChannels, height, width = image.shape
+    else:
+        raise ValueError("Unknown 'data_format' passed to 'keras_convolve2d_4dinput.")
+
+    if data_format.lower() == "channels_last":
+        conv2d_answer = np.zeros((height,width,filters))
+        for filter in np.arange(filters):
+            for channel in np.arange(nChannels):
+                id = np.ravel_multi_index((channel,filter),(nChannels,filters))
+                print(f"filter: {filter:2d}  channel: {channel:2d}  linearized index: {id:2d}")
+                conv2d_answer[:,:,filter] += keras_convolve2d(image[0,:,:,channel],kernel[0,:,:,id],strides,mode) # update zero index in first array position to handle batch_size
+    elif data_format.lower() == "channels_first":
+        conv2d_answer = np.zeros((filters,height,width))
+        for filter in np.arange(filters):
+            for channel in np.arange(nChannels):
+                id = np.ravel_multi_index((channel,filter),(nChannels,filters))
+                conv2d_answer[filter] += keras_convolve2d(image[0,channel,:,:],kernel[0,id,:,:],strides,mode) # update zero index in first array position to handle batch_size
+
+    return conv2d_answer[::strides[0],::strides[1]]
+
+def generate_keras_kernel(nRows,nCols,nFilters,nChannels):
+    '''
+        Generates an initial kernel (weights) for Keras 2D convolution layer. This essentially, creates an array from an integer sequence (1,2,3,4...) into a format 
+        acceptable for the Keras model. The reordering of the columns is so that Keras applies the kernels in (my desired) sequential integer sequence. For instance,
+        given two Filters with 2 kernels (channels) per filter given by
+
+        F1 = [[[1,2],[3,4]], [[5,6],[7,8]]]
+        F2 = [[[9,10],[11,12]], [[13,14],[15,16]]]
+
+        image = [[[1,2],[3,4]], [[5,6],[7,8]]]
+
+        To respect the filter's two kernels (channels), you must reorder the columns in the Keras kernel so that the first kernel in all filters come first then the second
+        kernel in all filters, all the way up to the last kernel in all filters.
+
+        Use the image and filters above, first filter applied to the image should give a 2D convolution result equal to [[184,112],[136,80]]. Similarly, the second filter
+        applied to the image gives a 2D convolution result equal to [[472,272],[312,176]].
+
+        Returns a 4d tensor
+    '''
+    column_permutations = np.concatenate((np.arange(0,nFilters*nChannels,2),np.arange(1,nFilters*nChannels,2)))
+    return np.arange(1,nFilters*nChannels*nRows*nCols+1).reshape(nRows*nCols,nFilters*nChannels,order='F')[:,column_permutations].reshape(1,nRows,nCols,nFilters*nChannels)
+
+def generate_mock_image(nRows,nCols,nChannels):
+    '''
+        Generates a mock image of integers in a sequence. The sequence is from 1 to nRows*nCols*nChannels.
+
+        Returns a 4d tensor
+    '''
+    return np.arange(1,nRows*nCols*nChannels+1).reshape(nRows*nCols,nChannels,order='F').reshape(1,nRows,nCols,nChannels)
--- tests/unit/utils/test_whetstone_utils.py
+++ tests/unit/utils/test_whetstone_utils.py
@@ -12,7 +12,7 @@ from fugu.utils.whetstone import whetstone_2_fugu
 
 import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, BatchNormalization
 from whetstone.layers import Spiking_BRelu
 from whetstone.utils import layer_utils
 
@@ -46,7 +46,7 @@ def keras_custom_model_inference():
     x = np.ones((1,2,2,1))
     features = feature_extractor(x)
 
-class Test_Whetstone_2_Fugu_Normalization_Off:
+class Test_Whetstone_2_Fugu:
     @pytest.mark.skip(reason="Not implemented. And takes to long for 'xfail'.")
     def test_layers(self):
         model = layer_utils.load_model(keras_mnist_model_norm_off())
@@ -153,71 +153,95 @@ class Test_Whetstone_2_Fugu_Normalization_Off:
         assert self.expected_spikes(nSpikes) == self.calculated_spikes(new_fugu_thresholds, result)
 
     def test_explicit_whetstone_2_fugu_conv2d_layer(self):
-            '''
-                [[1,2,3]                 [[ 4,11,18, 9]
-                [4,5,6]  *  [[1,2]  =     [18,37,47,21]
-                [7,8,9]]     [3,4]]       [36,67,77,33]
-                                          [14,23,26, 9]]
+        '''
+            [[1,2,3]                 [[ 4,11,18, 9]
+            [4,5,6]  *  [[1,2]  =     [18,37,47,21]
+            [7,8,9]]     [3,4]]       [36,67,77,33]
+                                        [14,23,26, 9]]
 
-                *** Note ***
-                [METHOD 1]
-                If you don't flip the kernel for Keras then don't "flip" the kernel when doing convolution by hand. Moreover this result is equivalent 
-                to performing scipy.signal.convolve2d([[1,2,3],[4,5,6],[7,8,9]],np.flip( [[1,2],[3,4]] ), mode="full").
+            *** Note ***
+            [METHOD 1]
+            If you don't flip the kernel for Keras then don't "flip" the kernel when doing convolution by hand. Moreover this result is equivalent 
+            to performing scipy.signal.convolve2d([[1,2,3],[4,5,6],[7,8,9]],np.flip( [[1,2],[3,4]] ), mode="full").
 
-                [METHOD 2]
-                Otherwise, "flip" the kernel (filter) for keras and then perform the traditional convolution practice when calculating convolution by hand.
-                The below result is equivalent to scipy.signal.convolve2d([[1,2,3],[4,5,6],[7,8,9]],[[1,2],[3,4]], mode="full")
+            [METHOD 2]
+            Otherwise, "flip" the kernel (filter) for keras and then perform the traditional convolution practice when calculating convolution by hand.
+            The below result is equivalent to scipy.signal.convolve2d([[1,2,3],[4,5,6],[7,8,9]],[[1,2],[3,4]], mode="full")
 
-                [[1,2,3]                 [[ 1, 4, 7, 6]
-                [4,5,6]  *  [[1,2]  =     [ 7,23,33,24]
-                [7,8,9]]     [3,4]]       [19,53,63,42]
-                                          [21,52,59,36]]
-            '''
-            from tensorflow.keras import Model, initializers
-            input_shape = (3,3,1)
-            strides = (1,1)
-            init_kernel = np.reshape(np.arange(1,5),(1,2,2,1))
-            init_bias = -52.6*np.ones((1,))
-            kernel_initializer = initializers.constant(np.flip(init_kernel)) # [METHOD 2] keras doesn't flip the filter during the convolution; so force the array flip manually.
-            bias_initializer = initializers.constant(init_bias)
-            nSpikes = 2
+            [[1,2,3]                 [[ 1, 4, 7, 6]
+            [4,5,6]  *  [[1,2]  =     [ 7,23,33,24]
+            [7,8,9]]     [3,4]]       [19,53,63,42]
+                                        [21,52,59,36]]
+        '''
+        from tensorflow.keras import Model, initializers
+        input_shape = (3,3,1)
+        strides = (1,1)
+        init_kernel = np.reshape(np.arange(1,5),(1,2,2,1))
+        init_bias = -52.6*np.ones((1,))
+        kernel_initializer = initializers.constant(np.flip(init_kernel)) # [METHOD 2] keras doesn't flip the filter during the convolution; so force the array flip manually.
+        bias_initializer = initializers.constant(init_bias)
+        nSpikes = 2
 
-            model = Sequential()
-            model.add(Conv2D(1, (2, 2), strides=strides, padding='same', activation=None, use_bias=True, input_shape=input_shape, name="one", kernel_initializer=kernel_initializer, bias_initializer=bias_initializer))
-            model.add(Spiking_BRelu(input_shape=(3,3),sharpness=1.0,name="spike"))
-            mock_image = np.arange(1,10,dtype=float).reshape(1,3,3,1)
-            calculated = model.layers[0](mock_image)[0,:,:,0].numpy().tolist() # gives the output of the input through this layer only
-            feature_extractor = Model(inputs=model.inputs, outputs=[layer.output for layer in model.layers]) # gives cumalative output of the input through this and previous layers.
+        model = Sequential()
+        model.add(Conv2D(1, (2, 2), strides=strides, padding='same', activation=None, use_bias=True, input_shape=input_shape, name="one", kernel_initializer=kernel_initializer, bias_initializer=bias_initializer))
+        model.add(Spiking_BRelu(input_shape=(3,3),sharpness=1.0,name="spike"))
+        mock_image = np.arange(1,10,dtype=float).reshape(1,3,3,1)
+        calculated = model.layers[0](mock_image)[0,:,:,0].numpy().tolist() # gives the output of the input through this layer only
+        feature_extractor = Model(inputs=model.inputs, outputs=[layer.output for layer in model.layers]) # gives cumalative output of the input through this and previous layers.
 
-            self.basep = 3
-            self.bits = 3
-            self.pvector = mock_image.reshape(3,3)
-            self.pshape = self.pvector.shape
-            self.filters = init_kernel.reshape(2,2)
-            self.filters_shape = self.filters.shape
-            self.strides = strides
-            scaffold = Scaffold()
-            scaffold.add_brick(BaseP_Input(mock_image.reshape(3,3),p=self.basep,bits=self.bits,collapse_binary=False,name="I",time_dimension=False),"input")
-            scaffold = whetstone_2_fugu(model,self.basep,self.bits,scaffold=scaffold)
-            scaffold.lay_bricks()
+        self.basep = 3
+        self.bits = 3
+        self.pvector = mock_image.reshape(3,3)
+        self.pshape = self.pvector.shape
+        self.filters = init_kernel.reshape(2,2)
+        self.filters_shape = self.filters.shape
+        self.strides = strides
+        scaffold = Scaffold()
+        scaffold.add_brick(BaseP_Input(mock_image.reshape(3,3),p=self.basep,bits=self.bits,collapse_binary=False,name="I",time_dimension=False),"input")
+        scaffold = whetstone_2_fugu(model,self.basep,self.bits,scaffold=scaffold)
+        scaffold.lay_bricks()
 
-            # Override output neuron threshold values in scaffold
-            output_neuron_names = np.array(scaffold.graph.nodes)[-9:]
-            keras_convolution_answer = feature_extractor(mock_image)[0][0,:,:,0].numpy()
-            keras_spike_answer = feature_extractor(mock_image)[1][0,:,:,0].numpy()
-            new_fugu_thresholds = (keras_convolution_answer - init_bias).astype(int) - 0.1*keras_spike_answer
+        # Override output neuron threshold values in scaffold
+        output_neuron_names = np.array(scaffold.graph.nodes)[-9:]
+        keras_convolution_answer = feature_extractor(mock_image)[0][0,:,:,0].numpy()
+        keras_spike_answer = feature_extractor(mock_image)[1][0,:,:,0].numpy()
+        new_fugu_thresholds = (keras_convolution_answer - init_bias).astype(int) - 0.1*keras_spike_answer
 
-            # Assign new thresholds values to convolution output neurons in the scaffold
-            for k, name in enumerate(output_neuron_names):
-                scaffold.graph.nodes[name]['threshold'] = new_fugu_thresholds.flatten()[k]
+        # Assign new thresholds values to convolution output neurons in the scaffold
+        for k, name in enumerate(output_neuron_names):
+            scaffold.graph.nodes[name]['threshold'] = new_fugu_thresholds.flatten()[k]
 
-            scaffold.summary(verbose=1)
-            backend = snn_Backend()
-            backend_args = {}
-            backend.compile(scaffold, backend_args)
-            result = backend.run(5)
+        scaffold.summary(verbose=1)
+        backend = snn_Backend()
+        backend_args = {}
+        backend.compile(scaffold, backend_args)
+        result = backend.run(5)
 
-            assert self.expected_spikes(nSpikes) == self.calculated_spikes(new_fugu_thresholds, result)
+        assert self.expected_spikes(nSpikes) == self.calculated_spikes(new_fugu_thresholds, result)
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_batch_normalization_removal(self):
+        from tensorflow.keras import Model, initializers
+        image_height, image_width = 3, 3
+        kernel_height, kernel_width = 2, 2
+        nChannels = 1
+        nFilters = 1
+        input_shape = (image_height,image_width,nChannels)
+        strides = (1,1)
+        init_kernel = generate_keras_kernel(kernel_height,kernel_width,nFilters,nChannels)
+        init_bias = -52.6*np.ones((nFilters,))
+        kernel_initializer = initializers.constant(np.flip(init_kernel)) # [METHOD 2] keras doesn't flip the filter during the convolution; so force the array flip manually.
+        bias_initializer = initializers.constant(init_bias)
+        nSpikes = 2
+
+        model = Sequential()
+        model.add(Conv2D(nFilters, (kernel_height, kernel_width), strides=strides, padding='same', activation=None, use_bias=True, input_shape=input_shape, name="one", kernel_initializer=kernel_initializer, bias_initializer=bias_initializer))
+        model.add(BatchNormalization())
+        model.add(Spiking_BRelu(input_shape=(image_height,image_width),sharpness=1.0,name="spike"))
+        mock_image = generate_mock_image(image_height,image_width,nChannels).astype(float)
+        calculated = model.layers[0](mock_image)[0,:,:,:].numpy().tolist() # gives the output of the input through this layer only
+        feature_extractor = Model(inputs=model.inputs, outputs=[layer.output for layer in model.layers]) # gives cumalative output of the input through this and previous layers.
+        assert False
 
     @pytest.mark.xfail(reason="Not implemented.")
     def test_4d_tensor_input_whetstone_2_fugu_conv2d_layer(self):
@@ -282,10 +306,6 @@ class Test_Whetstone_2_Fugu_Normalization_Off:
             expected_spikes = list(np.ones((nSpikes,)))
 
         return expected_spikes
-class Test_Whetstone_2_Fugu_Normalization_On:
-    @pytest.mark.xfail(reason="Not implemented.")
-    def test_layers(self):
-        assert False
 
 class Test_Whetstone_2_Fugu_PoolingLayer:
     @pytest.mark.xfail(reason="Not implemented.")
@@ -309,3 +329,75 @@ class Test_Whetstone_2_Fugu_DenseLayer:
     @pytest.mark.xfail(reason="Not implemented.")
     def test_dense_layer(self):
         assert False
+
+
+# Auxillary/Helper functions
+def keras_convolve2d(image,kernel,strides=(1,1),mode="same"):
+    '''
+        Custom function that uses scipy.signal.convove2d to reproduce the Keras 2D convolution result with strides
+    '''
+    return np.flip(convolve2d(np.flip(image),np.flip(kernel),mode=mode))[::strides[0],::strides[1]]
+
+def keras_convolve2d_4dinput(image,kernel,strides=(1,1),mode="same",data_format="channels_last",filters=1):
+    '''
+        Custom function that uses scipy.signal.convove2d to reproduce the Keras 2D convolution result with strides. Attempts to
+        handle the number of channels in image and the number of kernels per filter to calculating the result.
+    '''
+    if data_format.lower() == "channels_last":
+        if image.ndim == 3:
+            height, width, nChannels = image.shape
+        elif image.ndim == 4:
+            batch_size, height, width, nChannels = image.shape
+    elif data_format.lower() == "channels_first":
+        if image.ndim == 3:
+            nChannels, height, width = image.shape
+        elif image.ndim == 4:
+            batch_size, nChannels, height, width = image.shape
+    else:
+        raise ValueError("Unknown 'data_format' passed to 'keras_convolve2d_4dinput.")
+
+    if data_format.lower() == "channels_last":
+        conv2d_answer = np.zeros((height,width,filters))
+        for filter in np.arange(filters):
+            for channel in np.arange(nChannels):
+                id = np.ravel_multi_index((channel,filter),(nChannels,filters))
+                print(f"filter: {filter:2d}  channel: {channel:2d}  linearized index: {id:2d}")
+                conv2d_answer[:,:,filter] += keras_convolve2d(image[0,:,:,channel],kernel[0,:,:,id],strides,mode) # update zero index in first array position to handle batch_size
+    elif data_format.lower() == "channels_first":
+        conv2d_answer = np.zeros((filters,height,width))
+        for filter in np.arange(filters):
+            for channel in np.arange(nChannels):
+                id = np.ravel_multi_index((channel,filter),(nChannels,filters))
+                conv2d_answer[filter] += keras_convolve2d(image[0,channel,:,:],kernel[0,id,:,:],strides,mode) # update zero index in first array position to handle batch_size
+
+    return conv2d_answer[::strides[0],::strides[1]]
+
+def generate_keras_kernel(nRows,nCols,nFilters,nChannels):
+    '''
+        Generates an initial kernel (weights) for Keras 2D convolution layer. This essentially, creates an array from an integer sequence (1,2,3,4...) into a format 
+        acceptable for the Keras model. The reordering of the columns is so that Keras applies the kernels in (my desired) sequential integer sequence. For instance,
+        given two Filters with 2 kernels (channels) per filter given by
+
+        F1 = [[[1,2],[3,4]], [[5,6],[7,8]]]
+        F2 = [[[9,10],[11,12]], [[13,14],[15,16]]]
+
+        image = [[[1,2],[3,4]], [[5,6],[7,8]]]
+
+        To respect the filter's two kernels (channels), you must reorder the columns in the Keras kernel so that the first kernel in all filters come first then the second
+        kernel in all filters, all the way up to the last kernel in all filters.
+
+        Use the image and filters above, first filter applied to the image should give a 2D convolution result equal to [[184,112],[136,80]]. Similarly, the second filter
+        applied to the image gives a 2D convolution result equal to [[472,272],[312,176]].
+
+        Returns a 4d tensor
+    '''
+    column_permutations = np.concatenate((np.arange(0,nFilters*nChannels,2),np.arange(1,nFilters*nChannels,2)))
+    return np.arange(1,nFilters*nChannels*nRows*nCols+1).reshape(nRows*nCols,nFilters*nChannels,order='F')[:,column_permutations].reshape(1,nRows,nCols,nFilters*nChannels)
+
+def generate_mock_image(nRows,nCols,nChannels):
+    '''
+        Generates a mock image of integers in a sequence. The sequence is from 1 to nRows*nCols*nChannels.
+
+        Returns a 4d tensor
+    '''
+    return np.arange(1,nRows*nCols*nChannels+1).reshape(nRows*nCols,nChannels,order='F').reshape(1,nRows,nCols,nChannels)
