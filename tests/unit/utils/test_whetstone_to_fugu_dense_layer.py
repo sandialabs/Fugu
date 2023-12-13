10350b6db0269937920995bb55205f1a9e554154
Michael C. Krygier <mkrygie@sandia.gov>
2023-12-13 13:13:32 -0700

    Moving on to the whetstone to fugu pooling layer and dense layer unit tests. Added failing unit tests that must pass to provide confidence the implementation is correct.

--- tests/unit/utils/test_whetstone_to_fugu_dense_layer.py
+++ tests/unit/utils/test_whetstone_to_fugu_dense_layer.py
@@ -9,9 +9,11 @@ from fugu.bricks.dense_bricks import dense_layer_1d, dense_layer_2d
 from fugu.bricks.input_bricks import BaseP_Input, Vector_Input
 from fugu.bricks.pooling_bricks import pooling_1d, pooling_2d
 from fugu.scaffold import Scaffold
+from fugu.utils.keras_helpers import keras_convolve2d, keras_convolve2d_4dinput, generate_keras_kernel, generate_mock_image, keras_convolution2d_output_shape, keras_convolution2d_output_shape_4dinput
 from fugu.utils.whetstone import whetstone_2_fugu
 
 import pandas as pd
+from tensorflow.keras import Model, initializers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, BatchNormalization
 
@@ -20,5 +22,25 @@ layer_utils = pytest.importorskip("whetstone.utils", reason=f"Whetstone package 
 
 class Test_Whetstone_2_Fugu_DenseLayer:
     @pytest.mark.xfail(reason="Not implemented.")
-    def test_dense_layer(self):
+    def test_simple_keras_dense_layer(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_dense_layer(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_dense_layer_with_biases_without_activation(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_dense_layer_without_biases_without_activation(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_dense_layer_with_biases_with_activation(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_dense_layer_without_biases_with_activation(self):
         assert False
--- tests/unit/utils/test_whetstone_to_fugu_pooling_layer.py
+++ tests/unit/utils/test_whetstone_to_fugu_pooling_layer.py
@@ -9,19 +9,35 @@ from fugu.bricks.dense_bricks import dense_layer_1d, dense_layer_2d
 from fugu.bricks.input_bricks import BaseP_Input, Vector_Input
 from fugu.bricks.pooling_bricks import pooling_1d, pooling_2d
 from fugu.scaffold import Scaffold
+from fugu.utils.keras_helpers import keras_convolve2d, keras_convolve2d_4dinput, generate_keras_kernel, generate_mock_image, keras_convolution2d_output_shape, keras_convolution2d_output_shape_4dinput
 from fugu.utils.whetstone import whetstone_2_fugu
 
 import pandas as pd
+from tensorflow import constant as tf_constant
+from tensorflow.keras import Model, initializers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, BatchNormalization
 
 Spiking_BRelu = pytest.importorskip("whetstone.layers", reason=f"Whetstone package not installed. Skipping test file {__file__} because of module dependency.").Spiking_BRelu
 layer_utils = pytest.importorskip("whetstone.utils", reason=f"Whetstone package not installed. Skipping test file {__file__} because of module dependency.").layer_utils
 
-class Test_Whetstone_2_Fugu_PoolingLayer:
+class Test_Whetstone_2_Fugu_MaxPoolingLayer:
+    def setup_method(self):
+        image_height, image_width, nChannels = 3, 3, 2
+        kernel_height, kernel_width, nFilters = 2, 2, 3
+
+        self.mode = "same"
+        self.strides = (1,1)
+        self.padding = "same"
+
+        input_shape = (image_height,image_width,nChannels)
+        init_kernel = generate_keras_kernel(kernel_height,kernel_width,nFilters,nChannels)
+        init_bias = np.array([-320., -653., -786.]).reshape((nFilters,))
+        kernel_initializer = initializers.constant(np.flip(init_kernel,(0,1))) # [METHOD 2] keras doesn't flip the filter during the convolution; so force the array flip manually.
+        bias_initializer = initializers.constant(init_bias)
+
     @pytest.mark.xfail(reason="Not implemented.")
     def test_simple_keras_max_pooling_layer(self):
-        from tensorflow.keras import Model, initializers
         input_shape = (3,3,1)
         strides = (1,1)
         init_kernel = np.reshape(np.flip(np.arange(1,5)),(1,2,2,1)) # [METHOD 2] keras doesn't flip the filter during the convolution; so force the array flip manually.
@@ -34,4 +50,83 @@ class Test_Whetstone_2_Fugu_PoolingLayer:
         model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),input_shape=input_shape))
         mock_image = np.arange(1,10,dtype=float).reshape(1,3,3,1)
         calculated = model.layers[0](mock_image)[0,:,:,0].numpy().tolist()
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_max_pooling_layer_same_padding_strides_11_pool_size_22(self):
+        image_height, image_width, nChannels = 3, 3, 2
+        kernel_height, kernel_width, nFilters = 2, 2, 3
+        pool_height, pool_width = 2, 2
+
+        pool_strides = (1,1)
+        pool_size = (pool_height, pool_width)
+        pool_padding = "same"
+
+        input_shape = (image_height,image_width,nChannels)
+        mock_image = generate_mock_image(image_height,image_width,nChannels).astype(float)
+
+        model = Sequential()
+        model.add(MaxPooling2D(pool_size=pool_size, strides=pool_strides, padding=pool_padding, input_shape=input_shape))
+        feature_extractor = Model(inputs=model.inputs, outputs=[layer.output for layer in model.layers]) # gives cumalative output of the input through this and previous layers.
+        features = feature_extractor(mock_image)
+
+        basep = 3
+        bits = 4
+        result = run_whetstone_to_fugu_utility(mock_image,basep,bits,model)
+
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_max_pooling_layer_same_padding_strides_12_pool_size_22(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_max_pooling_layer_same_padding_strides_21_pool_size_22(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_max_pooling_layer_same_padding_strides_22_pool_size_22(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_max_pooling_layer_same_padding_strides_12_pool_size_32(self):
+        assert False
+
+
+def run_whetstone_to_fugu_utility(mock_image, basep, bits, keras_model):
+    scaffold = Scaffold()
+    scaffold.add_brick(BaseP_Input(mock_image,p=basep,bits=bits,collapse_binary=False,name="I",time_dimension=False),"input")
+    scaffold = whetstone_2_fugu(keras_model,basep,bits,scaffold=scaffold)
+    scaffold.lay_bricks()
+    scaffold.summary(verbose=1)
+    backend = snn_Backend()
+    backend_args = {}
+    backend.compile(scaffold, backend_args)
+    result = backend.run(5)
+    return result
+
+
+class Test_Whetstone_2_Fugu_AveragePoolingLayer:
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_simple_keras_average_pooling_layer(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_average_pooling_layer_same_padding_strides_11_pool_size_22(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_average_pooling_layer_same_padding_strides_12_pool_size_22(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_average_pooling_layer_same_padding_strides_21_pool_size_22(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_average_pooling_layer_same_padding_strides_22_pool_size_22(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented.")
+    def test_keras_average_pooling_layer_same_padding_strides_12_pool_size_32(self):
         assert False
